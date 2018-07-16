04738bf3cfa819d2ae1f0d3044694f1aea958593
DA\nwuser <noppo.kappa@gmail.com>
2018-07-17 05:56:18 +0900

    【Game】クリア演出追加・シーン遷移

---
+++ MergeManGene/Assets/Scenes/SandBox/Kaihatsu/Scripts/GoalPerformance.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class GoalPerformance : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject m_goalFrame;
+    private AudioSource m_audioSource;
+
+    [SerializeField]
+    private AudioClip m_goalSE;
+
+	// Use this for initialization
+	void Start () {
+        m_audioSource = GetComponent<AudioSource>();
+        m_audioSource.clip = m_goalSE;
+	}
+
+
+
+    void OnTriggerEnter2D(Collider2D arg_col){
+
+        if (arg_col.tag == "Player"){
+            if (!m_audioSource.isPlaying) { m_audioSource.Play(); }
+            Instantiate(m_goalFrame, new Vector3(72f, 0.32f, 0), Quaternion.identity);
+            StartCoroutine("Change");
+
+        }
+    }
+
+    private IEnumerator Change(){
+        yield return new WaitForSeconds(3f);
+        FadeManager.Instance.LoadScene("GameEnd", 1.5f);
+
+    }
+}
