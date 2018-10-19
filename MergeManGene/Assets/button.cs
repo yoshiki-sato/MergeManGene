4d0643523187a12cbe6c9568717685acf2e6e7a6
DA\nwuser <puzz_ryu_777@ezweb.ne.jp>
2018-10-19 11:07:38 +0900

    ちんぱんぷっしゅ

---
+++ MergeManGene/Assets/button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("へい");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
